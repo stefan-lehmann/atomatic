02393b16284ce093b3b90ba8ebdefdb94296d3d2
Stefan Lehmann <stefan@lehmann.st>
2019-01-28 22:45:50 +0100

    enable SelfAdjustingIframeComponent to calculate iframe height for components that make use of margin

--- app/scripts/components/SelfAdjustingIframeComponent/index.js
+++ app/scripts/components/SelfAdjustingIframeComponent/index.js
@@ -1,6 +1,7 @@
 import VueComponent from '../../vue/VueComponent';
 import templateFn from './template.pug';
 
+
 class SelfAdjustingIframeComponent extends VueComponent {
 
   props() {
@@ -24,11 +25,6 @@ class SelfAdjustingIframeComponent extends VueComponent {
     };
   }
 
-  onUrlChange() {
-    const {[this.url]: {height = 0} = {height: 0}} = this.$store.getters.iframeStates;
-    this.setHeight(height);
-  }
-
   mounted() {
     this.onUrlChange();
   }
@@ -37,22 +33,22 @@ class SelfAdjustingIframeComponent extends VueComponent {
     window.removeEventListener('resize', this.updateHeight);
   }
 
-  updateHeight() {
-    const {document} = this.$refs.iframe.contentWindow;
+  getHeightFromChildNodes(document) {
+    return [...document.body.childNodes]
+      .filter((child) => child.offsetHeight)
+      .map((child) => this.getHeightWithMargin(child))
+      .sort((a, b) => a > b)
+      .shift();
+  }
 
-    if (document && document.body) {
-      let height = document.body.offsetHeight || this.getHeightFromChildNodes(document) || 200;
-      if (this.height === height) {
-        this.$store.commit('setIframeState', {url: this.url, height});
-        window.addEventListener('resize', this.updateHeight);
-        return;
-      }
-      this.setHeight(height);
-    }
-    setTimeout(this.updateHeight, 100);
-    setTimeout(this.updateHeight, 500);
-    setTimeout(this.updateHeight, 1000);
-    setTimeout(this.updateHeight, 2000);
+  getHeightWithMargin(element) {
+    const {marginBottom, marginTop} = getComputedStyle(element);
+    return Number.parseInt(marginTop) + Number.parseInt(marginBottom) + Number.parseInt(element.offsetHeight);
+  }
+
+  onUrlChange() {
+    const {[this.url]: {height = 0} = {height: 0}} = this.$store.getters.iframeStates;
+    this.setHeight(height);
   }
 
   setHeight(height = 0) {
@@ -60,12 +56,27 @@ class SelfAdjustingIframeComponent extends VueComponent {
     this.$refs.iframe.style.height = `${height}px`;
   }
 
-  getHeightFromChildNodes(document) {
-    return [...document.body.childNodes]
-      .filter((child) => child.offsetHeight)
-      .map((child) => child.offsetHeight)
-      .sort((a, b) => a > b)
-      .shift();
+  updateHeight() {
+    const {iframe: {contentWindow: {document, document: {body} = {}} = {}} = {}} = this.$refs;
+
+    if (body) {
+      let height = this.getHeightFromChildNodes(document) || 200;
+
+      if (this.height === height) {
+        this.$store.commit('setIframeState', {
+          url: this.url,
+          height
+        });
+        window.addEventListener('resize', this.updateHeight);
+        return;
+      }
+      this.setHeight(height);
+    }
+
+    setTimeout(this.updateHeight, 100);
+    setTimeout(this.updateHeight, 500);
+    setTimeout(this.updateHeight, 1000);
+    setTimeout(this.updateHeight, 2000);
   }
 }
 
